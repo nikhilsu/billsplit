794c2d67f6d28f854513b5ffc8d0f765ca02cd80
nikhilsu <nikhilsu@thoughtworks.com>
2016-01-29 00:51:17 +0530

    Added test and implementation for getAllCheckedCheckBoxes

--- src/js/index-script.js
+++ src/js/index-script.js
@@ -1,3 +1,11 @@
 function getAllCheckedCheckboxes(nameOfCheckboxElement) {
-    return ['Riju', 'Varsha'];
+    var checkBoxes = document.getElementsByName(nameOfCheckboxElement);
+    var checkedCheckBoxes = [];
+
+    for (var i=0;i<checkBoxes.length;i++){
+        if (checkBoxes[i].checked){
+            checkedCheckBoxes.push(checkBoxes[i].value);
+        }
+    }
+    return checkedCheckBoxes;
 }
--- test/jsTest/index-script-test.js
+++ test/jsTest/index-script-test.js
@@ -1,12 +1,48 @@
 var assert = require('assert');
+var sinon = require('sinon'); //used for stubbing.
+var jsdom = require('jsdom-global'); //used for getting the global document object.
 require('node-import'); //used to import javascript files.
 imports('src/js/index-script.js');
 
 describe('index-script', function() {
     describe('getAllCheckedCheckboxes()', function () {
 
-        it('should return the names of all the checkboxes which are checked', function () {
+        function createMockHTMLCheckBox(name, checked) {
+            var mockHTMLCheckboxElement = document.createElement('input');
+            mockHTMLCheckboxElement.type = ('checkbox');
+            mockHTMLCheckboxElement.name = 'people';
+            mockHTMLCheckboxElement.value = name;
+            mockHTMLCheckboxElement.checked = checked;
+            return mockHTMLCheckboxElement;
+        }
+
+        before(function() {
+            jsdom(); //fetches the document object.
+            sinon.stub(document, 'getElementsByName');
+        });
+
+        it('should return the names of the checked checkboxes', function () {
+            var mockHTMLCheckboxes = [];
+            var mockHTMLRijuCheckbox = createMockHTMLCheckBox('Riju', true);
+            var mockHTMLVarshaCheckbox = createMockHTMLCheckBox('Varsha', true);
+            var mockHTMLThejasCheckbox = createMockHTMLCheckBox('Thejas', false);
+            mockHTMLCheckboxes.push(mockHTMLRijuCheckbox, mockHTMLThejasCheckbox, mockHTMLVarshaCheckbox);
+            document.getElementsByName.withArgs('people').returns(mockHTMLCheckboxes);
             var expectedValues = ['Riju', 'Varsha'];
+
+            var actualValues = getAllCheckedCheckboxes('people');
+
+            assert.deepEqual(expectedValues, actualValues);
+        });
+
+        it('should return the names of all the checkboxes which are checked', function () {
+            var mockHTMLCheckboxes = [];
+            var mockHTMLSPCheckbox = createMockHTMLCheckBox('SP', true);
+            var mockHTMLKusumCheckbox = createMockHTMLCheckBox('Kusum', false);
+            var mockHTMLMuskanCheckbox = createMockHTMLCheckBox('Muskan', true);
+            mockHTMLCheckboxes.push(mockHTMLSPCheckbox, mockHTMLKusumCheckbox, mockHTMLMuskanCheckbox);
+            document.getElementsByName.withArgs('people').returns(mockHTMLCheckboxes);
+            var expectedValues = ['SP', 'Muskan'];
 
             var actualValues = getAllCheckedCheckboxes('people');
 
